0038feebe987be25b1ed9580cbb88fb171f2337a
Rex Liang <159222539+Rex0Liang@users.noreply.github.com>
2025-03-26 12:08:21 +1100

    Changes to input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,15 +11,16 @@ public GameObject winTextObject;
 public GameObject loseTextObject;
 public float speed = 0;
 public TextMeshProUGUI countText; 
-private Rigidbody rb; 
+private Rigidbody PlayerRB; 
 private int count;
 private float movementX;
 private float movementY;
+public float jumpHeight;    
 
     void Start()
     {
         winTextObject.SetActive(false);
-        rb = GetComponent <Rigidbody>(); 
+        PlayerRB = GetComponent <Rigidbody>(); 
         count = 0; 
         SetCountText();
     }
@@ -29,40 +30,50 @@ private float movementY;
         movementX = movementVector.x; 
         movementY = movementVector.y; 
     }
+
+    void Update(InputValue jumpValue) 
+    {
+        Vector3 jumping = new Vector3 (0, jumpHeight, 0;);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayerRB.AddForce(jumpHeight);
+        }
+    }
     
     void SetCountText() 
     {
         countText.text =  "Count: " + count.ToString();
         if (count >= 11)
-       {
-           winTextObject.SetActive(true);
-           Destroy(GameObject.FindGameObjectWithTag("Enemy"));
-       }
+        {
+            winTextObject.SetActive(true);
+            Destroy(GameObject.FindGameObjectWithTag("Enemy"));
+        }
     }
     private void FixedUpdate() 
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
 
-        rb.AddForce(movement * speed); 
+        PlayerRB.AddForce(movement * speed); 
     }
 
-   void OnTriggerEnter(Collider other) 
-   {
-           if (other.gameObject.CompareTag("PickUp")) 
-       {
-         other.gameObject.SetActive(false);
-         count = count + 1;
-         SetCountText();
-       }
-   }
+    void OnTriggerEnter(Collider other) 
+    {
+            if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
 
-   private void OnCollisionEnter(Collision collision)
-   {
+    private void OnCollisionEnter(Collision collision)
+    {
         if(collision.gameObject.CompareTag("Enemy"))
         {   
         Destroy(gameObject);
         winTextObject.gameObject.SetActive(true);
         winTextObject.GetComponent<TextMeshProUGUI>().text = "You lose!";
         }
-   }
+    }
 }
